6836b44b12c65aecd0a12e52ea9536264d155bb2
上田将司 <uedamasashi@uedamasashinoMacBook-Air.local>
2021-08-16 23:10:57 +0900

    タブでの画面変更

--- frontend/src/components/swipe.jsx
+++ frontend/src/components/swipe.jsx
@@ -1,3 +1,10 @@
+// import React from "react";
+// import SwipeableViews from "react-swipeable-views";
+// import { makeStyles } from "@material-ui/core/styles";
+
+// import Tabs from "@material-ui/core/Tabs";
+// import Tab from "@material-ui/core/Tab";
+
 import React from "react";
 import SwipeableViews from "react-swipeable-views";
 
@@ -12,46 +19,88 @@ import LivingTV from "./pages/LivingTV"
 import LivingAirConditionar from "./pages/LivingAirConditionar";
 
 
-const Swipe=(props) =>{
+const Swipe = (props) => {
     const [swipeableActions, setSwipeableActions] = React.useState();
     const [tabIndex, setTabIndex] = React.useState(0);
-
 
     const handleChange = index => {
         setTabIndex(index);
     };
+
     const myIo = props.myIo;
 
     const tabNames = ["リビング\nテレビ","リビング\nエアコン"]
-    return (
-        <React.Fragment>
-            <Tabs
-                value={tabIndex}
-                onChange={(e, value) => handleChange(value)}
-                variant="fullWidth"
-                indicatorColor="primary"
-            >
-                {tabNames.map((tabName, index)=>(
-                    <Tab 
-                    label={
-                        <Typography style={{whiteSpace: 'pre-line'}}>
-                            {tabName}
-                        </Typography>
-                    }/>
-                ))}
-            </Tabs>
-            <SwipeableViews
-                enableMouseEvents
-                action={actions => setSwipeableActions(actions)}
-                resistance
-                animateHeight
-            >
-                <LivingTV myIo = {myIo}/>
-                <LivingAirConditionar />
-            </SwipeableViews>
-        </React.Fragment>
 
-    );
+  return (
+    <React.Fragment>
+      <Tabs
+        value={tabIndex}
+        onChange={(e, value) => handleChange(value)}
+        variant="fullWidth"
+        indicatorColor="primary"
+      >
+        {tabNames.map((tabName, index)=>(
+            <Tab 
+            label={
+            <Typography style={{whiteSpace: 'pre-line'}}>
+                {tabName}
+            </Typography>
+            }/>
+        ))}
+      </Tabs>
+      <SwipeableViews
+        enableMouseEvents
+        action={actions => setSwipeableActions(actions)}
+        resistance
+        animateHeight
+        index={tabIndex}
+        onChangeIndex={index => handleChange(index)}
+      >
+        <LivingTV myIo = {myIo}/>
+        <LivingAirConditionar />
+      </SwipeableViews>
+    </React.Fragment>
+  );
 };
-
 export default Swipe;
+
+
+
+
+
+//     const myIo = props.myIo;
+
+//     const tabNames = ["リビング\nテレビ","リビング\nエアコン"]
+//     return (
+//         <React.Fragment>
+//             <Tabs
+//                 value={tabIndex}
+//                 onChange={(e, value) => handleChange(value)}
+//                 variant="fullWidth"
+//                 indicatorColor="primary"
+//             >
+//                 {tabNames.map((tabName, index)=>(
+//                     <Tab 
+//                     label={
+//                         <Typography style={{whiteSpace: 'pre-line'}}>
+//                             {tabName}
+//                         </Typography>
+//                     }/>
+//                 ))}
+//             </Tabs>
+//             <SwipeableViews
+//                 enableMouseEvents
+//                 action={actions => setSwipeableActions(actions)}
+//                 resistance
+//                 animateHeight
+//                 onChangeIndex={index => handleChange(index)}
+//             >
+//                 <LivingTV myIo = {myIo}/>
+//                 <LivingAirConditionar />
+//             </SwipeableViews>
+//         </React.Fragment>
+
+//     );
+// };
+
+// export default Swipe;
